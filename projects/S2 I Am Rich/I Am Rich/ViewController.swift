db859a3f3e240e537b6e7adbb0bc7af20827bc12
Andrew Gentile <agentile1990@gmail.com>
2020-04-18 21:13:32 -0500

    S2 I Am Rich

---
+++ projects/S2 I Am Rich/I Am Rich/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  I Am Rich
+//
+//  Created by Andrew Gentile on 4/17/20.
+//  Copyright © 2020 Andrew Gentile. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
