7b73887cf473daae6a3f5326b1cb840ec7990310
Andrew Gentile <agentile1990@gmail.com>
2020-04-20 20:05:50 -0500

    S3 I Am Poor

---
+++ projects/S3 I Am Poor/I Am Poor/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  I Am Poor
+//
+//  Created by Andrew Gentile on 4/20/20.
+//  Copyright © 2020 Andrew Gentile. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+  override func viewDidLoad() {
+    super.viewDidLoad()
+    // Do any additional setup after loading the view.
+  }
+
+
+}
+
