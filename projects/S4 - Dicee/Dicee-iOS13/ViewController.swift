d5f1d3b740147778fcdd918067a5d21e825476a6
Andrew Gentile <agentile1990@gmail.com>
2020-04-26 16:14:14 -0500

    S4 Dicee

---
+++ projects/S4 - Dicee/Dicee-iOS13/ViewController.swift
@@ -0,0 +1,28 @@
+//
+//  ViewController.swift
+//  Dicee-iOS13
+//
+//  Created by Angela Yu on 11/06/2019.
+//  Copyright © 2019 London App Brewery. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+  
+  // Reference to dice UI elements
+  @IBOutlet weak var diceImageView1: UIImageView!
+  @IBOutlet weak var diceImageView2: UIImageView!
+  
+  let images = [#imageLiteral(resourceName: "DiceOne"), #imageLiteral(resourceName: "DiceTwo"), #imageLiteral(resourceName: "DiceThree"), #imageLiteral(resourceName: "DiceFour"), #imageLiteral(resourceName: "DiceFive"), #imageLiteral(resourceName: "DiceSix")]
+  
+  var leftDiceNumber = Int.random(in: 0...5)
+  var rightDiceNumber = Int.random(in: 0...5)
+  
+
+  @IBAction func rollButtonPressed(_ sender: UIButton) {
+    diceImageView1.image = images.randomElement()
+    diceImageView2.image = images.randomElement()
+  }
+}
+
